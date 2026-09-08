46b9f50966628a519c9b0e8962f87e9c781a65e0
Perean561 <brian060501@gmail.com>
2026-09-08 17:07:15 -0600

    First Commit

---
+++ src/main/java/com/tecnm/merida/market_backendv2/MarketBackendv2Application.java
@@ -0,0 +1,13 @@
+package com.tecnm.merida.market_backendv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketBackendv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketBackendv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecnm/merida/market_backendv2/MarketBackendv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnm.merida.market_backendv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketBackendv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
